6072f65099c80db2a48b5aac833450db82e36a21
Thanh Duc Nguyen <thanh.nguyenduc@hellopay.com>
2016-06-05 13:51:27 +0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   get 'users/index' => 'users/index'
   get 'microposts/index'
   get 'static_pages/home'
+
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   root 'courses#index'
 
   resources :users
