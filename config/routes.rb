f8be06d9963a7927e262efd4253ab934bea2fab4
Thanh Duc Nguyen <thanh.nguyenduc@hellopay.com>
2016-06-07 09:13:23 +0700

    add sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
 
   resources :users
   resources :microposts
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606012322) do
+ActiveRecord::Schema.define(version: 20160606154103) do
 
   create_table "microposts", force: :cascade do |t|
     t.string   "content"
