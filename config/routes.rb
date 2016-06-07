a7edd9aa533aca0ead54179d608be6a95adad617
Thanh Nguyen <thanh.nguyen-duc@hellopay.com>
2016-06-07 14:12:07 +0700

    add log out feature

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   match '/signout', to: 'sessions#destroy',     via: 'delete'
 
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
